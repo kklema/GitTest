8460a826275d22380c0e04b6d2973b5718e5f265
kklema <ownedrus@gmail.com>
2021-05-05 01:12:01 +0300

    Сделал авто уменьшение/увеличение громкости сирены

--- Assets/Scripts/Door/AlarmZone.cs
+++ Assets/Scripts/Door/AlarmZone.cs
@@ -7,11 +7,13 @@ public class AlarmZone : MonoBehaviour
     private AudioSource _audioAlarm;
     private AlarmLamp _colorAlarm;
     private float _alarmTimer;
+    private bool _isVolumeIncreased;
 
     private void Start()
     {
         _audioAlarm = GetComponent<AudioSource>();
         _colorAlarm = GetComponentInChildren<AlarmLamp>();
+        _audioAlarm.volume = 0;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,6 +31,7 @@ public class AlarmZone : MonoBehaviour
         {
             _audioAlarm.Stop();
             _audioAlarm.volume = 0;
+            _alarmTimer = 0;
             _colorAlarm.StopChangeColor();
         }
     }
@@ -37,10 +40,30 @@ public class AlarmZone : MonoBehaviour
     {
         if (collision.TryGetComponent<Enemy>(out Enemy enemy))
         {
+            float maxVolume = 1f;
+            float minVolume = 0f;
+            _alarmTimer += Time.deltaTime / 5;
+
             if (_audioAlarm.isPlaying)
             {
-                _alarmTimer += Time.deltaTime / 1000;
                 _audioAlarm.volume += _alarmTimer;
+                if (_audioAlarm.volume >= maxVolume && _isVolumeIncreased == true)
+                {
+                    _isVolumeIncreased = false;
+                }
+                else if (_isVolumeIncreased == false)
+                {
+                    _audioAlarm.volume += -_alarmTimer;
+                    if (_audioAlarm.volume <= minVolume)
+                    {
+                        _isVolumeIncreased = true;
+                    }
+                }
+
+                if (_alarmTimer > maxVolume)
+                {
+                    _alarmTimer = minVolume;
+                }
             }
         }
     }
