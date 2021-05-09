4ad5049e73594052d43806eef735720d2b087c89
kklema <ownedrus@gmail.com>
2021-05-09 21:49:44 +0300

    Many changes
    rename scenes
    add spawn scene
    add enemy sprites
    manage code style

---
+++ Assets/Scripts/Enemy/Flying.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Flying : MonoBehaviour
+{
+    [SerializeField] float _speed;
+
+    private Rigidbody2D _rigidody2D;
+
+    private void Start()
+    {
+        _rigidody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Flip();
+        _rigidody2D.velocity = new Vector2(_speed, 0);
+    }
+
+    private void Flip()
+    {
+        if (_speed > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (_speed < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
