d0046050d8268114b20371c6db47ee5fa3bbd88d
kklema <ownedrus@gmail.com>
2021-05-15 21:23:03 +0300

    домашку переделал
    movement переименовал в patrol
    сделал отдельный метод для Flip но только для крысы
    во второй сцене спавнера у глаза убрал поворот из скрипта, т.к. движение происходит по-другому.

--- Assets/Scripts/Enemy/Flip.cs
+++ Assets/Scripts/Enemy/Flip.cs
@@ -2,33 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class Flip : MonoBehaviour
 {
-    private SpriteRenderer _spriteRenderer;
-    private Rigidbody2D _rb2D;
-
-    private float _speed;
+    private bool _isFacingRight;
 
     private void Start()
     {
-        _rb2D = GetComponent<Rigidbody2D>();
+        _isFacingRight = true;
     }
 
-    private void Update()
+    public void TurnSide(Vector3 newPosition)
     {
-        TurnSide();
-    }
-
-    private void TurnSide()
-    {
-        if (_rb2D.velocity.x > 0f)
-        {
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (_rb2D.velocity.x < 0f)
+        if (newPosition.x < transform.position.x && _isFacingRight)
         {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
+            _isFacingRight = false;
+        }
+        else if (newPosition.x > transform.position.x && !_isFacingRight)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            _isFacingRight = true;
         }
     }
 }
--- Assets/Scripts/Enemy/Flying.cs
+++ Assets/Scripts/Enemy/Flying.cs
@@ -16,19 +16,6 @@ public class Flying : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Flip();
         _rigidody2D.velocity = new Vector2(_speed, 0);
-    }
-
-    private void Flip()
-    {
-        if (_speed > 0)
-        {
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (_speed < 0)
-        {
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
-        }
     }
 }
